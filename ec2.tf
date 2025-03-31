f9cf2304ccae56cbc76146392132732797cf4c5e
tamilarasu-g <tamilaguru08150@gmail.com>
2025-03-31 10:20:32 +0530

    removed userdata since its not working

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "instance-1" {
     subnet_id = aws_subnet.pub-subnet-1.id
     security_groups = [aws_security_group.ec2_public_sg.id]
     key_name = var.key-name
-    user_data = templatefile("frontend.sh.tftpl", {backend_lb_url = aws_lb.tta_internal_lb.dns_name })
+    # user_data = templatefile("frontend.sh.tftpl", {backend_lb_url = aws_lb.tta_internal_lb.dns_name })
 
     tags = merge(local.common_tags,{
         Name = var.instance_1_name
@@ -20,7 +20,7 @@ resource "aws_instance" "instance-2" {
     subnet_id = aws_subnet.pub-subnet-2.id
     security_groups = [aws_security_group.ec2_public_sg.id]
     key_name = var.key-name
-    user_data = templatefile("frontend.sh.tftpl", {backend_lb_url = aws_lb.tta_internal_lb.dns_name })
+    # user_data = templatefile("frontend.sh.tftpl", {backend_lb_url = aws_lb.tta_internal_lb.dns_name })
 
 
     tags = merge(local.common_tags,{
@@ -36,7 +36,7 @@ resource "aws_instance" "instance-3" {
     subnet_id = aws_subnet.private_subnet_1.id
     security_groups = [aws_security_group.ec2_private_sg.id]
     key_name = var.key-name
-    user_data = templatefile("backend.sh.tftpl", { db_host=aws_db_instance.tta_db.address, db_username=var.db_username, db_password = var.db_password, db_name = "users" })
+    # user_data = templatefile("backend.sh.tftpl", { db_host=aws_db_instance.tta_db.address, db_username=var.db_username, db_password = var.db_password, db_name = "users" })
 
     tags = merge(local.common_tags,{
         Name = var.instance_3_name
@@ -49,7 +49,7 @@ resource "aws_instance" "instance-4" {
     subnet_id = aws_subnet.private_subnet_2.id
     security_groups = [aws_security_group.ec2_private_sg.id]
     key_name = var.key-name
-    user_data = templatefile("backend.sh.tftpl", { db_host=aws_db_instance.tta_db.address, db_username=var.db_username, db_password = var.db_password, db_name = "users" })
+    # user_data = templatefile("backend.sh.tftpl", { db_host=aws_db_instance.tta_db.address, db_username=var.db_username, db_password = var.db_password, db_name = "users" })
 
     tags = merge(local.common_tags,{
         Name = var.instance_4_name
